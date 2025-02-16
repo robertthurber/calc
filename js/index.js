3a5993ee9fa95185fd7ff1bc0717bce0d1f82c3d
Robert <83424654+robertthurber@users.noreply.github.com>
2025-02-16 17:35:34 -0500

    added the math function along with the operate logic

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// function for adding number
+function addNum(num1, num2) {
+  let addNumber = num1 + num2;
+  return addNumber;
+}
+
+// function for subtracting numbers
+function subtractingNum(num1, num2) {
+  let subNumber = num1 - num2;
+  return subNumber;
+}
+
+//function for multiplying number
+function multiplyingNum(num1, num2) {
+  let multiplyNum = num1 * num2;
+  return multiplyNum;
+}
+
+//function for dividing numbers
+function dividingNum(num1, num2) {
+  let divNum = parseFloat(num1) / parseFloat(num2);
+  return divNum;
+}
+
+// variables for each option a user can do on the calculator
+let userNum1 = 0;
+let userNum2 = 0;
+let operator = "";
+
+function operate(userNum1, userNum2, operator) {
+  if (operator == "+") {
+    return addNum(userNum1, userNum2);
+  } else if (operator == "-") {
+    return subtractingNum(userNum1, userNum2);
+  } else if (operator == "*") {
+    return multiplyingNum(userNum1, userNum2);
+  } else if (operator == "/") {
+    return dividingNum(userNum1, userNum2);
+  }
+}
